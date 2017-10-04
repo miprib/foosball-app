c3eb00e958c92e01e7233068a9e70753bf6c5af5
Mikas <mikas.prib@gmail.com>
2017-10-04 16:12:41 +0300

    Initial commit
    Added a simple menu and a launcher icon.

---
+++ Foosball/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace Foosball
+{
+    [Activity(Label = "Foosball", MainLauncher = true, Icon = "@drawable/foosball_launcher")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get the UI controls from the loaded layout:
+            Button startButton = FindViewById<Button>(Resource.Id.StartButton);
+
+            startButton.Click += (object sender, EventArgs e) =>
+            {
+                // TO-DO: add code for "Start"
+            };
+
+        }
+    }
+}
+
