f74e3e9bb6dd5e4c3657a8a9d43346c2f4402a57
Justinas Šideikis <justinasidk@gmail.com>
2017-10-18 23:05:28 +0300

    Bandau sumergint savaip, nes kitu būdu labai problematiška

---
+++ VidTests/GameListTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Vid.Tests
+{
+    [TestClass]
+    public class GameListTests
+    {
+        [TestMethod]
+        public void FilePathTest()
+        {
+            // Arrange
+            String regexPattern = @"^([\w]\:)(\\[A-z_\-\s0-9\.]+)+\.(txt)$";
+
+            // Act
+            GameList.NewInstance();
+            String actual = GameList.path;
+
+            // Assert
+            Assert.IsTrue(Regex.IsMatch(actual, regexPattern));
+        }
+
+    }
+}
