001a72ab8965e87202da0a0d5571744a4b35cb6c
Aidas <aidas.saltis@gmail.com>
2017-11-06 22:07:45 +0200

    Added a textureview and live camera feed. Its now possible to retrieve frames.

---
+++ VidTests1/GameListTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vid.Tests
+{
+    [TestClass()]
+    public class GameListTests
+    {
+        [TestMethod()]
+        public void SaveListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- xamarin-android/MainActivity.cs
+++ xamarin-android/MainActivity.cs
@@ -4,24 +4,31 @@ using Android.OS;
 using Android.Views;
 using Android.Content;
 using System;
-using System.Drawing;
 
+using System.Threading;
 using Emgu.CV;
 using Emgu.CV.Structure;
-using System.Threading;
+using Android.Graphics;
+using Android.Content.PM;
+using Camera = Android.Hardware.Camera;
+using Xamarin.Forms;
 
 namespace xamarin_android
 {
-    [Activity(Label = "xamarin_android", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "xamarin_android", MainLauncher = true, ConfigurationChanges = ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.Landscape)]
+    public class MainActivity : Activity , TextureView.ISurfaceTextureListener
     {
         VideoCapture capture;
         ImageView imageView;
 
+        private Camera camera;
+        TextureView textureView;
+
         MCvScalar sk = new MCvScalar();
-        Point po = new Point(-1, -1);
-        Size sz = new Size(3, 3);
-        Mat s = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle, new Size(3, 3), new Point(-1, -1));
+        //Point po = new Point(-1, -1);
+        //Size sz = new Size(3, 3);
+       // Mat s = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle, new Size(3, 3), new Point(-1, -1));
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -29,16 +36,20 @@ namespace xamarin_android
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
-   
-            imageView = FindViewById<ImageView> (Resource.Id.imageView);
+
+            textureView = new TextureView(this);
+            textureView.SurfaceTextureListener = this;
+            SetContentView(textureView);
+
+            /*imageView = FindViewById<ImageView> (Resource.Id.imageView);
             Button button1 = FindViewById<Button>(Resource.Id.button);
             button1.Click += (sender, e) => {
                 // Perform action on click
                 var imageIntent = new Intent();
-                imageIntent.SetType("image/*");
+                imageIntent.SetType("video/*");
                 imageIntent.SetAction(Intent.ActionGetContent);
                 StartActivityForResult(Intent.CreateChooser(imageIntent, "Select photo"), 0);
-            };
+            };*/
         }
        
 
@@ -48,7 +59,7 @@ namespace xamarin_android
 
             {
             if (resultCode == Result.Ok)
-                capture = new VideoCapture(data.DataString);
+                capture = new VideoCapture(AppDomain.CurrentDomain.BaseDirectory + "\\..\\..\\" + "\\data\\video.mp4");
                 imageView.SetImageURI(data.Data);
             }
             if (capture != null)
@@ -72,9 +83,9 @@ namespace xamarin_android
 
                 CvInvoke.InRange(g, new ScalarArray(new MCvScalar(0, 100, 100)), new ScalarArray(new MCvScalar(10, 255, 255)), g);  // išskiriam raudona spalva per tas tris eilutes
 
-                CvInvoke.Blur(g, g, sz, po); // išryškinam paveikslėlį
-                CvInvoke.Dilate(g, g, s, po, 1, Emgu.CV.CvEnum.BorderType.Default, sk);
-                CvInvoke.Erode(g, g, s, po, 1, Emgu.CV.CvEnum.BorderType.Default, sk);
+                //CvInvoke.Blur(g, g, sz, po); // išryškinam paveikslėlį
+                //CvInvoke.Dilate(g, g, s, po, 1, Emgu.CV.CvEnum.BorderType.Default, sk);
+               // CvInvoke.Erode(g, g, s, po, 1, Emgu.CV.CvEnum.BorderType.Default, sk);
 
 
                 CircleF[] circles = CvInvoke.HoughCircles(g, Emgu.CV.CvEnum.HoughType.Gradient, 2, g.Rows / 4, 60, 30, 15, 40); // ieškom apvalių(kažkodėl ir ne tik) objektų jau toj išskirtoj raudonoj spalvoj
@@ -85,7 +96,7 @@ namespace xamarin_android
                 {
                     string text = "ball position = x " + circle.Center.X.ToString() + ", y " + circle.Center.Y.ToString() + System.Environment.NewLine; //dvi eilut4s tekstui
                     //textBox1.Invoke(new Action(() => textBox1.AppendText(text))); // reikia kreiptis taip, nes is kito threado negalima toliau test visko
-                    circleImage.Draw(circle, new Bgr(Color.Red), 4); // apibrėžiam apvalius
+                    //circleImage.Draw(circle, new Bgr(Color.Red), 4); // apibrėžiam apvalius
                 }
 
                 //pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; // šitie du del dydžio lango dydžio, kad viskas matytuos
@@ -93,13 +104,55 @@ namespace xamarin_android
 
                 //pictureBox1.Image = g.ToImage<Bgr, byte>().Bitmap;
                 imageView.SetImageBitmap(circleImage.Bitmap);
-                imageView.Image = circleImage.Bitmap;
+                //imageView.Image = circleImage.Bitmap;
                 Thread.Sleep((int)capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps));
             }
             catch (Exception)
             {
 
             }
+        }
+        /** method that opens camera when the activity is launched
+         */
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+        {
+            camera = Camera.Open();
+
+            try
+            {
+                camera.SetPreviewTexture(surface);
+                camera.StartPreview();
+            }
+            catch (Exception t)
+            {}
+
+            var metrics = Resources.DisplayMetrics;
+
+            Camera.Parameters parameters = camera.GetParameters();
+            parameters.SetPreviewSize((int)metrics.WidthPixels, (int)metrics.HeightPixels);
+            parameters.FocusMode = Camera.Parameters.FocusModeContinuousPicture;
+        }
+
+        public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
+        {
+            if (camera != null)
+            {
+                camera.StopPreview();
+                camera.Release();
+                camera = null;
+            }
+            return false;
+        }
+
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+        {
+        }
+
+        /** method that can gets called with every frame. retrieve frame with 
+         *  Bitmap frameBitmap = textureView.Bitmap;
+         */
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
         }
     }
 }
