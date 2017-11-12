cf8b09e35e329cc0415d525828d956bca9b3578d
Justinas Šideikis <justinasidk@gmail.com>
2017-11-12 15:30:55 +0200

    Drawing circle around ball working

--- xamarin-android/MainActivity.cs
+++ xamarin-android/MainActivity.cs
@@ -13,18 +13,20 @@ using Android.Content.PM;
 using Camera = Android.Hardware.Camera;
 using Xamarin.Forms;
 using Android.Media;
+using Android.Runtime;
 
 namespace xamarin_android
 {
     [Activity(Label = "xamarin_android", MainLauncher = true, ConfigurationChanges = ConfigChanges.Orientation,
         ScreenOrientation = ScreenOrientation.Landscape)]
-    public class MainActivity : Activity , TextureView.ISurfaceTextureListener
+    public class MainActivity : Activity , TextureView.ISurfaceTextureListener, ISurfaceHolderCallback
     {
         VideoCapture capture;
         ImageView imageView;
 
         private Camera camera;
         ISurfaceHolder surfaceHolder;
+        SurfaceView surfaceView;
         TextureView textureView;
         MediaPlayer mediaPlayer;
 
@@ -40,19 +42,19 @@ namespace xamarin_android
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
 
-            textureView = new TextureView(this);
+            textureView = (TextureView)FindViewById(Resource.Id.textureView1);
             textureView.SurfaceTextureListener = this;
-            SetContentView(textureView);
 
-            SurfaceView surfaceView = (SurfaceView)FindViewById(Resource.Id.surfaceView);
+            surfaceView = (SurfaceView)FindViewById(Resource.Id.surfaceView);
             //set to top layer
             surfaceView.SetZOrderOnTop(true);
             //set the background to transparent
             surfaceView.Holder.SetFormat(Format.Transparent);
             surfaceHolder = surfaceView.Holder;
+            surfaceHolder.AddCallback(this);
+
         }
        
-
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
@@ -169,13 +171,65 @@ namespace xamarin_android
          */
         public void OnSurfaceTextureUpdated(SurfaceTexture surface)
         {
-            //draw
-            Canvas canvas = surfaceHolder.LockCanvas();
-            //clear the paint of last time
-            canvas.DrawColor(Color.Transparent, PorterDuff.Mode.Clear);
-            //draw a new one, set your ball's position to the rect here
-            canvas.DrawCircle(x, y, radius, mpaint);
-            surfaceHolder.UnlockCanvasAndPost(canvas);
+            
+            
+                Bitmap frame = textureView.Bitmap;
+                Image<Bgr, byte> fr = new Image<Bgr, byte>(frame);
+                Mat m = fr.Mat;
+
+                Mat ball = Detailing.Detail(m);
+
+                CircleF[] circles = CvInvoke.HoughCircles(ball, Emgu.CV.CvEnum.HoughType.Gradient, 2, ball.Rows / 4, 60, 30, 15, 40); // ieškom apvalių(kažkodėl ir ne tik) objektų jau toj išskirtoj raudonoj spalvoj
+
+                Paint paint = new Paint();
+                paint.Color = Android.Graphics.Color.Red;
+                paint.SetStyle(Paint.Style.Stroke);
+                paint.StrokeWidth = 5f;
+
+                if (surfaceCreated)
+                {
+                    Canvas canvas = surfaceHolder.LockCanvas();
+                    //clear the paint of last time
+                    canvas.DrawColor(Android.Graphics.Color.Transparent, PorterDuff.Mode.Clear);
+                    //draw a new one, set your ball's position to the rect here
+                
+                    surfaceHolder.UnlockCanvasAndPost(canvas);
+                }
+        
+                foreach (CircleF circle in circles)
+                {
+                    string text = "ball position = x " + circle.Center.X.ToString() + ", y " + circle.Center.Y.ToString() + System.Environment.NewLine;
+                    Console.Write(text);
+
+                    //draw
+
+                    Console.WriteLine(surfaceCreated);
+                    if (surfaceCreated)
+                    {
+                        Canvas canvas = surfaceHolder.LockCanvas();
+                        //clear the paint of last time
+                     
+                        //draw a new one, set your ball's position to the rect here
+                        canvas.DrawCircle(circle.Center.X, circle.Center.Y, circle.Radius, paint);
+                        surfaceHolder.UnlockCanvasAndPost(canvas);
+                    }
+                }
+            
+            
+        }
+
+        public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height)
+        {
+        }
+        bool surfaceCreated = false;
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            surfaceCreated = true;
+            Console.WriteLine("called");
+        }
+
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
         }
     }
 }
