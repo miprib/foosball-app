44fce9e596749a651ca8815dacdb381c87e131d2
Mikas <mikas.prib@gmail.com>
2017-11-15 18:26:15 +0200

    Unit Tests
    Added some Unit Tests to chech if the specified URL is correct, and to check if the specified port is 5000.

---
+++ xamarin-androidTests2/ServerConnectionTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xamarin_android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace xamarin_android.Tests
+{
+    [TestClass()]
+    public class ServerConnectionTests
+    {
+        [TestMethod]
+        public void UrlTest()
+        {
+            // Arrange
+            String regexPattern = @"^((http[s]?|ftp):\/)?\/?([^:\/\s]+)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$";
+
+            // Act
+            String url = ServerConnection.url;
+
+            // Assert
+            Assert.IsTrue(Regex.IsMatch(url, regexPattern));
+        }
+
+        [TestMethod]
+        public void PortTest()
+        {
+            // Arrange
+            String regexPattern = @"^((http[s]?|ftp):\/)?\/?([^:\/\s]+)+([\w\-\.]+[^#?\s]+)(5000)(.*)?(#[\w\-]+)?$";
+
+            // Act
+            String url = ServerConnection.url;
+
+            // Assert
+            Assert.IsTrue(Regex.IsMatch(url, regexPattern));
+        }
+    }
+}
